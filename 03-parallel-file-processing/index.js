b9fd00451a9e3a05bb6daa76130bac8dc1ace6b8
Logan Absher <absherlogan@gmail.com>
2018-06-25 17:06:55 -0700

    created new file reader method that returns the first 8 bytes

--- 03-parallel-file-processing/index.js
+++ 03-parallel-file-processing/index.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const libModules = require('./lib/fileReader.js');
+
+libModules.fileReader(`${__dirname}/assets/one.txt`);
+libModules.fileReader(`${__dirname}/assets/two.txt`);
+libModules.fileReader(`${__dirname}/assets/three.txt`);
--- 03-parallel-file-processing/lib/fileReader.js
+++ 03-parallel-file-processing/lib/fileReader.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const fs = require('fs');
+
+module.exports = exports = {};
+
+exports.fileReader = function (file) {
+  fs.readFile(file, function (err, data) {
+    if (err) throw err;
+    console.log(Buffer.alloc(8, data, 'hex'));
+  });
+};
